a9f62ff0cc0b15f57eaaa8c335f4751f8ff30cbd
omaymaoun <131709586+omaymaoun@users.noreply.github.com>
2025-05-01 13:19:34 +0100

    Initial commit

--- src/Controller/ReclamationController.php
+++ src/Controller/ReclamationController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\TranslateService; 
 
+
 #[Route('/reclamation')]
 final class ReclamationController extends AbstractController
 {
@@ -33,7 +34,6 @@ final class ReclamationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $reclamation->setUser($this->getUser());
             $entityManager->persist($reclamation);
             $entityManager->flush();
             $this->addFlash('success', 'Réclamation envoyée avec succès.');
